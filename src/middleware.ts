36cf89fae33b81df321282d0684a3ed77b85213f
kle1603 <bkhang160303@gmail.com>
2024-09-04 09:12:16 +0700

    create - maintenance page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const maintenanceMode = true
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
+  if (maintenanceMode) {
+    const url = new URL('/maintenance', request.url)
+    return NextResponse.redirect(url)
+  }
+
+  return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/test/:path*'
+  matcher: ['/', '/about', '/contact', '/course', '/course/[slug]', '/knowledge', '/knowledge/[slug]']
 }
