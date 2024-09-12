439fef5a8b67c29541c65e559a41c31dce90486c
kle1603 <bkhang160303@gmail.com>
2024-09-12 19:42:05 +0700

    fix: style home page

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -7,15 +7,8 @@ import Link from 'next/link'
 function Hero() {
   return (
     <section className='container pt-[60px] md:pt-[100px] grid grid-cols-1 lg:grid-cols-2 h-[100vh]'>
-      <div className='relative mr-0 lg:mr-10 flex justify-center items-center'>
-        <Image
-          src={images.heroBackground}
-          alt='Hero background'
-          width={1920}
-          height={1080}
-          className='absolute top-10 left-0 object-contain -z-10'
-        />
-        <div className='w-full flex items-center flex-col'>
+      <div className='mr-0 lg:mr-10 flex justify-center items-center'>
+        <div className='w-full flex items-center flex-col relative'>
           <Image src={images.cloud} alt='Cloud' width={500} height={500} className='md:max-w-[420px]' />
 
           <h1 className='mt-6 max-w-[420px] text-lg font-medium text-center break-words text-black/65'>
@@ -33,6 +26,14 @@ function Hero() {
           <Button asChild className='mt-4 h-14 w-full md:w-[420px] bg-third hover:bg-third/90 text-lg rounded-2xl'>
             <Link href={configRoute.course}>Đăng ký các khóa học ngay</Link>
           </Button>
+
+          <Image
+            src={images.heroBackground}
+            alt='Hero background'
+            width={1920}
+            height={1080}
+            className='absolute bottom-40 right-5 object-contain -z-10'
+          />
         </div>
       </div>
       <div className='w-[calc(50vw-4px)] hidden lg:block'>
